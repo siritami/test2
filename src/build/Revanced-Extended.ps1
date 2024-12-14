87a7811b8f28dd3f4ee0f8e349371d0e0264b4c3
siritami <103488094+siritami@users.noreply.github.com>
2024-12-14 18:13:50 +0700

    1

--- src/build/Revanced-Extended.ps1
+++ src/build/Revanced-Extended.ps1
@@ -10,9 +10,11 @@ function patch {
     if (Test-Path $apkPath) {
         java -jar revanced-cli.jar patch -p patches.rvp --legacy-options=$patchOption --keystore=src/_ks.keystore -o "release\$apkName-$patchOption.apk" --purge $apkPath
     } else {
-        Write-Host "[-] Not found $apkPath"
+        Write-Host "[-] Not found $apkName"
         exit 1
     }
 }
-
+if (-not (Test-Path "release")) {
+    New-Item -Path "release" -ItemType Directory
+}
 patch "youtube" "revanced-extended"
