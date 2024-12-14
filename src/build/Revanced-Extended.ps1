051ca068f2cd30d2ce8203bc318aca46b4da4aa6
siritami <103488094+siritami@users.noreply.github.com>
2024-12-15 01:19:30 +0700

    1

--- src/build/Revanced-Extended.ps1
+++ src/build/Revanced-Extended.ps1
@@ -15,10 +15,10 @@ function patch {
 New-Item -Path "release" -ItemType Directory -Force | Out-Null
 
 Invoke-WebRequest -Uri "https://cdn.azul.com/zulu/bin/zulu17.54.21-ca-jdk17.0.13-win_x64.msi" -OutFile "zulu.msi"
-Invoke-WebRequest -Uri "https://github.com/AutoHotkey/AutoHotkey/releases/download/v2.0.18/AutoHotkey_2.0.18.zip" -OutFile "AutoHotkey.zip"
+Invoke-WebRequest -Uri "https://github.com/AutoHotkey/AutoHotkey/releases/download/v2.0.18/AutoHotkey_2.0.18.zip" -OutFile "src\build\AutoHotkey.zip"
 
 msiexec /i zulu.msi ADDLOCAL=FeatureJavaHome,FeatureEnvironment /qn
 Expand-Archive -Path "AutoHotkey.zip" -DestinationPath "." -Force
-Start-Process "AutoHotkey64.exe" -ArgumentList "D:\a\test2\test2\src\build\Revanced-Extended.ahk"
+Start-Process "AutoHotkey64.exe" -ArgumentList "Revanced-Extended.ahk"
 
 #patch "youtube" "revanced-extended"
