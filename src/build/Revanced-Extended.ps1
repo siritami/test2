bca13e82762fcd0f96b649f78ce65700acd6ac86
siritami <103488094+siritami@users.noreply.github.com>
2024-12-14 23:37:23 +0700

    1

--- src/build/Revanced-Extended.ps1
+++ src/build/Revanced-Extended.ps1
@@ -1,46 +1,17 @@
-Set-ExecutionPolicy RemoteSigned
-
 function patch {
     param(
         [string]$apkName,
         [string]$alias
     )
-    
     $apkPath = "$apkName.apk"
-	$javaPath = Get-ChildItem -Path "download\zulu-jdk-win_x64\Program Files\Zulu\zulu*\bin\java.exe" -Recurse | Select-Object -First 1
     if (Test-Path $apkPath) {
-        if ($javaPath) {
-            & $javaPath -jar revanced-cli.jar patch -p patches.rvp --legacy-options=$alias --keystore=src/_ks.keystore -o "release\$apkName-$alias.apk" --purge $apkPath
-        } else {
-            Write-Host "[-] Java executable not found."
-            exit 1
-        }
+        java -jar revanced-cli.jar patch -p patches.rvp --legacy-options=$alias -o "release\$apkName-$alias.apk" $apkPath
     } else {
         Write-Host "[-] Not found $apkName"
         exit 1
     }
 }
-function setup-zulu {
-	$Url = "https://cdn.azul.com/zulu/bin/zulu17.54.21-ca-jdk17.0.13-win_x64.msi"
-	$DownloadPath = "download\zulu-jdk-win_x64.msi"
-	$ExtractPath = "download\zulu-jdk-win_x64"
-	$Directory = [System.IO.Path]::GetDirectoryName($DownloadPath)
-	if (-not (Test-Path -Path $Directory)) {
-		New-Item -ItemType Directory -Path $Directory
-	}
-	$ExtractDirectory = [System.IO.Path]::GetDirectoryName($ExtractPath)
-	if (-not (Test-Path -Path $ExtractDirectory)) {
-		New-Item -ItemType Directory -Path $ExtractDirectory
-	}
-	Invoke-WebRequest -Uri $Url -OutFile $DownloadPath
-	Write-Host "Download complete: $DownloadPath"
-	Start-Process msiexec.exe -ArgumentList "/a `"$DownloadPath`" /qb TARGETDIR=`"$ExtractPath`"" -NoNewWindow -Wait
-	Write-Host "Extraction complete: $ExtractPath"
-
-}
-
 if (-not (Test-Path "release")) {
     New-Item -Path "release" -ItemType Directory -Force | Out-Null
 }
-setup-zulu
 patch "youtube" "revanced-extended"
