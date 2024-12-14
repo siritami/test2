8ab0d87d4f557975a25d38235f6bb3cc129821d8
siritami <103488094+siritami@users.noreply.github.com>
2024-12-14 16:54:38 +0700

    1

--- src/build/Revanced-Extended.sh
+++ src/build/Revanced-Extended.sh
@@ -11,7 +11,11 @@ revanced_dl(){
 	revanced_dl
 	# Patch YouTube:
 	get_patches_key "youtube-revanced-extended"
-	get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube" "Bundle"
+	get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
+	split_editor "youtube" "youtube"
+	mv src/build/*.rvp download/patches.rvp
+	mv src/build/*.jar download/cli.jar
+	rm -f download/youtube.apkm
 }
 2() {
 	revanced_dl
