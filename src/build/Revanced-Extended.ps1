61f9ff092ccf21dc73b9b960d45f6c4912ce9779
siritami <103488094+siritami@users.noreply.github.com>
2024-12-14 18:02:54 +0700

    Update Revanced-Extended.ps1

--- src/build/Revanced-Extended.ps1
+++ src/build/Revanced-Extended.ps1
@@ -8,7 +8,7 @@ function patch {
     
     $apkPath = "$apkName.apk"
     if (Test-Path $apkPath) {
-        java -jar revanced-cli.jar -p patches.rvp --legacy-options=$patchOption --keystore=src/_ks.keystore -o "release\$apkName-$patchOption.apk" --purge $apkPath
+        java -jar revanced-cli.jar patch -p patches.rvp --legacy-options=$patchOption --keystore=src/_ks.keystore -o "release\$apkName-$patchOption.apk" --purge $apkPath
     } else {
         Write-Host "[-] Not found $apkPath"
         exit 1
