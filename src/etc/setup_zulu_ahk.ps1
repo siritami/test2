e3bd0b220aa39f317a74517e5d35ded27e9a2782
siritami <103488094+siritami@users.noreply.github.com>
2024-12-15 02:40:08 +0700

    Update setup_zulu_ahk.ps1

--- src/etc/setup_zulu_ahk.ps1
+++ src/etc/setup_zulu_ahk.ps1
@@ -1,15 +1,3 @@
-function Update-EnvironmentVariables {
-'Start-Process', [Diagnostics.Process]::GetCurrentProcess().ProcessName,
-'-UseNewEnvironment -NoNewWindow -Wait -Args ''-c'',',
-'''Get-ChildItem env: | &{process{ $_.Key + [char]9 + $_.Value }}'''
-))) | &{process{
-  [Environment]::SetEnvironmentVariable(
-    $_.Split("`t")[0],
-    $_.Split("`t")[1],
-    'Process'
-  )
-}}
-}
 
 New-Item -Path "release" -ItemType Directory -Force | Out-Null
 
@@ -19,4 +7,3 @@ Invoke-WebRequest -Uri "https://github.com/AutoHotkey/AutoHotkey/releases/downlo
 msiexec /i zulu.msi ADDLOCAL=FeatureJavaHome,FeatureEnvironment /qn
 Expand-Archive -Path "D:\a\test2\test2\AutoHotkey.zip" -DestinationPath "." -Force
 Start-Process "D:\a\test2\test2\AutoHotkey64.exe" -ArgumentList "D:\a\test2\test2\src\build\Revanced-Extended.ahk"
-Update-EnvironmentVariables
