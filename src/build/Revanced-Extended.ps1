c46721fd8557b11a8f9a1a92dc58070941222d3e
siritami <103488094+siritami@users.noreply.github.com>
2024-12-15 00:11:53 +0700

    1

--- src/build/Revanced-Extended.ps1
+++ src/build/Revanced-Extended.ps1
@@ -14,4 +14,8 @@ function patch {
 if (-not (Test-Path "release")) {
     New-Item -Path "release" -ItemType Directory -Force | Out-Null
 }
+
+Invoke-WebRequest -Uri "https://cdn.azul.com/zulu/bin/zulu17.54.21-ca-jdk17.0.13-win_x64.msi" -OutFile "zulu.msi"
+msiexec /i zulu.msi ADDLOCAL=FeatureJavaHome,FeatureEnvironment /qn
+
 patch "youtube" "revanced-extended"
