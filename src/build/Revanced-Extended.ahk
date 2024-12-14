a9f1c8d6ad017f3fba0aed03a6bdf2a2c4a80fb9
siritami <103488094+siritami@users.noreply.github.com>
2024-12-15 00:31:11 +0700

    1

---
+++ src/build/Revanced-Extended.ahk
@@ -0,0 +1,12 @@
+﻿; Open Command Prompt
+Run "cmd.exe"
+WinWaitActive "ahk_class ConsoleWindowClass"
+
+; Type the first command and press Enter
+Send "cd D:\a\test2\test2\{Enter}"
+
+; Type the second command and press Enter
+Send "java -jar revanced-cli.jar patch -p patches.rvp -o youtube-revanced-extended.apk youtube.apk{Enter}"
+
+; Stop the script
+ExitApp
--- src/build/Revanced-Extended.ps1
+++ src/build/Revanced-Extended.ps1
@@ -16,6 +16,10 @@ if (-not (Test-Path "release")) {
 }
 
 Invoke-WebRequest -Uri "https://cdn.azul.com/zulu/bin/zulu17.54.21-ca-jdk17.0.13-win_x64.msi" -OutFile "zulu.msi"
+Invoke-WebRequest -Uri "https://www.autohotkey.com/download/ahk-v2.exe" -OutFile "AutoHotKey.exe"
+
 msiexec /i zulu.msi ADDLOCAL=FeatureJavaHome,FeatureEnvironment /qn
+AutoHotKey.exe /silent /Elevate
+Start-Process "C:\Program Files\AutoHotkey\AutoHotkey.exe" -ArgumentList "D:\a\test2\test2\src\build\Revanced-Extended.ahk"
 
 #patch "youtube" "revanced-extended"
