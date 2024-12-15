4bbd225e5dce373ec4a51daea198a045d3e3a16e
siritami <103488094+siritami@users.noreply.github.com>
2024-12-15 12:25:46 +0700

    Update setup_zulu_ahk.ps1

--- src/etc/setup_zulu_ahk.ps1
+++ src/etc/setup_zulu_ahk.ps1
@@ -5,6 +5,4 @@ Invoke-WebRequest -Uri "https://cdn.azul.com/zulu/bin/zulu17.54.21-ca-jdk17.0.13
 Invoke-WebRequest -Uri "https://github.com/AutoHotkey/AutoHotkey/releases/download/v2.0.18/AutoHotkey_2.0.18.zip" -OutFile "D:\a\test2\test2\AutoHotkey.zip"
 
 msiexec /i zulu.msi ADDLOCAL=FeatureJavaHome,FeatureEnvironment /qn
-Start-Process cmd -ArgumentList "/c timeout /t 2 && exit"
-Start-Sleep -s 4
 Expand-Archive -Path "D:\a\test2\test2\AutoHotkey.zip" -DestinationPath "." -Force
